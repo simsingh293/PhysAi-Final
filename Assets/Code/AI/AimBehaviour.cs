40544d7d3ba69e8280eece9a64a4a5be1f54fd0b
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-03-14 08:41:36 -0400

    Initial commit

---
+++ Assets/Code/AI/AimBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimBehaviour : MonoBehaviour
+{
+    public Transform lookAtTarget = null;
+    public Rigidbody rigid = null;
+
+    public TargetGetterBehaviour targetGetter = null;
+
+
+    private Quaternion originalRotation = Quaternion.Euler(Vector3.zero);
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        originalRotation = rigid.rotation;
+
+        targetGetter = GetComponentInParent<TargetGetterBehaviour>();
+    }
+
+    void Update()
+    {
+        if(lookAtTarget != null)
+        {
+            ChangeRotation();
+        }
+    }
+
+    void ChangeRotation()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 targetPosition = lookAtTarget.position;
+        Vector3 directionOfTarget = targetPosition - currentPosition;
+
+        rigid.MoveRotation(Quaternion.LookRotation(directionOfTarget));
+    }
+
+    public void AddTarget()
+    {
+        if(lookAtTarget == null)
+        {
+            lookAtTarget = targetGetter.targetTransform;
+        }
+    }
+
+    public void RemoveTarget()
+    {
+        if(lookAtTarget != null)
+        {
+            lookAtTarget = null;
+            rigid.MoveRotation(originalRotation);
+        }
+    }
+}
---
+++ Assets/Code/AI/TargetGetterBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetGetterBehaviour : MonoBehaviour
+{
+    public UnityEvent onTargetFound = new UnityEvent();
+    public UnityEvent onTargetLost = new UnityEvent();
+
+    public Transform targetTransform = null;
+
+    float gizmoRadius = 0;
+
+    void Start()
+    {
+        gizmoRadius = GetComponent<SphereCollider>().radius;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Found");
+            targetTransform = other.gameObject.transform;
+            
+            onTargetFound?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(targetTransform != null && other.gameObject.transform == targetTransform)
+        {
+            Debug.Log("Player out of Range");
+            targetTransform = null;
+            onTargetLost?.Invoke();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, gizmoRadius);
+    }
+}
